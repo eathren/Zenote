e2dceed12de7eddf8b61436dd438b6fb2f137203
nolanbraman <nolan.braman@gmail.com>
2023-10-22 18:29:24 -0700

    fixed menu overflow

--- src/components/UI/Headers/NodeHeader.tsx
+++ src/components/UI/Headers/NodeHeader.tsx
@@ -1,26 +1,25 @@
 import NodeMenu from "../../NodeMenu"
 import NodeControls from "../../NodeControls"
 import BasicHeader from "./BasicHeader"
-import { Button, Col, Input, Row, Typography } from "antd"
-import { EditOutlined } from "@ant-design/icons"
+import { Button, Col, Input, Row } from "antd"
+import { EditOutlined, BookOutlined } from "@ant-design/icons"
 import { useState } from "react"
 
 type NodeHeaderProps = {
   editableTitle: string | undefined
   onTitleChange: (e: React.ChangeEvent<HTMLInputElement>) => void
   toggleEditMode: () => void
+  editMode: boolean
 }
 
 const commonStyle = {
   border: "none",
   outline: "none",
   fontFamily: "inherit",
-  fontWeight: 600,
-  fontSize: "1.5rem",
 }
 
 const NodeHeader = (props: NodeHeaderProps) => {
-  const [isTitleEditable, setIsTitleEditable] = useState<boolean>(false)
+  const [isTitleEditable] = useState<boolean>(false)
   const { editableTitle, onTitleChange, toggleEditMode } = props
 
   return (
@@ -35,28 +34,27 @@ const NodeHeader = (props: NodeHeaderProps) => {
           <Col md={2} sm={4} xs={4}>
             <NodeMenu />
           </Col>
-          <Col md={10} sm={8} xs={10}>
-            {isTitleEditable ? (
-              <Input
-                value={editableTitle}
-                onChange={onTitleChange}
-                autoFocus
-                style={commonStyle}
-              />
-            ) : (
-              <Typography.Title
-                style={commonStyle}
-                level={4}
-                onClick={() => setIsTitleEditable(!isTitleEditable)}
-              >
-                {editableTitle}
-              </Typography.Title>
-            )}
+          <Col md={10} sm={8} xs={10} style={{ maxWidth: "100%" }}>
+            <Input
+              value={editableTitle}
+              onChange={onTitleChange}
+              autoFocus
+              style={{
+                ...commonStyle,
+                maxWidth: "100%",
+                width: "100%",
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+              }}
+            />
           </Col>
           <Col md={12} sm={12} xs={10}>
             <Row justify="end" align="middle" gutter={16}>
               <Col>
-                <Button onClick={toggleEditMode} icon={<EditOutlined />} />
+                <Button
+                  onClick={toggleEditMode}
+                  icon={isTitleEditable ? <EditOutlined /> : <BookOutlined />}
+                />
               </Col>
               <Col>
                 <NodeControls />
