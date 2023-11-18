63ff590de5d639a97ab63106df7c7d647a6563ba
nolanbraman <nolan.braman@gmail.com>
2023-11-17 18:18:33 -0800

    add a graph

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,54 @@
+import { Card } from "antd"
+import { PlusCircleOutlined } from "@ant-design/icons"
 import { Spin } from "antd"
 import useGraphs from "src/hooks/useGraphs"
+import { GraphPrivacySetting } from "src/types"
+import AddGraphButton from "src/components/Graphs/AddGraphButton"
 
 const HomePage = () => {
-  const { loading } = useGraphs()
+  const { graphs, loading } = useGraphs()
+
   if (loading)
     return <Spin style={{ position: "absolute", top: "50%", left: "50%" }} />
-  return <></>
+
+  const hasNoGraphs = graphs.length === 0
+
+  return (
+    <div style={{ height: "100vh", position: "relative" }}>
+      {!loading && hasNoGraphs && (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+          }}
+        >
+          <Card
+            style={{ width: 300 }}
+            cover={
+              <div style={{ padding: "20px", textAlign: "center" }}>
+                <PlusCircleOutlined
+                  style={{ fontSize: "48px", color: "#1890ff" }}
+                />
+              </div>
+            }
+            actions={[
+              <AddGraphButton type={GraphPrivacySetting.Private}>
+                Create Private Graph
+              </AddGraphButton>,
+            ]}
+          >
+            <Card.Meta
+              title="No Graphs Yet"
+              description="Start by creating your first graph."
+            />
+          </Card>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default HomePage
