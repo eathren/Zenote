991703cfe55f1c708dbfe7b67abffc7bebfc8ac7
Nolan Braman <nolan.braman@gmail.com>
2023-11-27 16:06:50 -0800

    pwa-ified

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa"
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  manifest: {
+    name: "Zenote",
+    short_name: "Zenote",
+    description:
+      "Zenote is a note-taking app designed to mimic how your brain works.",
+    theme_color: "#141414",
+    display: "standalone",
+    icons: [
+      {
+        src: "/public/Zenote.svg",
+        sizes: "192x192",
+        type: "image/svg+xml",
+      },
+      {
+        src: "/public/Zenote.svg",
+        sizes: "512x512",
+        type: "image/svg+xml",
+      },
+    ],
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: {
       src: "/src",
     },
   },
-});
+})
